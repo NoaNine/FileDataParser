b4be6ec9dc7c58278795de7659f4e7a765f0ed20
Oleksandr Havlytskyi <havlytskyi@gmail.com>
2023-04-26 16:58:43 +0300

    added Agregator class implementation and returns its own type with the needful data

--- Math/Aggregator.cs
+++ Math/Aggregator.cs
@@ -2,9 +2,39 @@
 {
     public class Aggregator
     {
-        public int[] Maximum(double[][] data)
+        public AggregatorResult Maximum(double[][] data)
         {
-            return new int[0];
+            AggregatorResult result = new AggregatorResult();
+            for(int i  = 0; i < data.Length; i++)
+            {
+                if (data[i] == null)
+                {
+                    result.BrokenRows.Add(i);
+                    continue;
+                }
+                double sum = GetSumAllElements(data[i]);
+                if (result.Value < sum)
+                {
+                    result.Value = sum;
+                    result.FoundInRows.Add(i);
+                    continue;
+                }
+                if(result.Value == sum)
+                {
+                    result.FoundInRows.Add(i);
+                }
+            }
+            return result;
+        }
+
+        private double GetSumAllElements(double[] data)
+        {
+            double sum = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                sum += data[i];
+            }
+            return sum;
         }
     }
 }
---
+++ Math/AggregatorResult.cs
@@ -0,0 +1,9 @@
+﻿namespace Math
+{
+    public class AggregatorResult
+    {
+        public double Value { get; set; }
+        public List<int> FoundInRows { get; set; }
+        public List<int> BrokenRows { get; set; }
+    }
+}
